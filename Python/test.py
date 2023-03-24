7fc35113e71b954f88373f186b3c47c1abfd52ea
Gajalakshmi <gajalakshmi@anorasolutions.com>
2023-03-24 09:45:46 +0530

    Test py file added

---
+++ Python/test.py
@@ -0,0 +1,7 @@
+# Chidambaram-Add function
+# Vivekanand -Sub Function
+# Yuvashree - Multiplication
+# Rajakeerthana - Division
+# Hitesh - Square of a number
+# Kumarsing - Cube of a number
+# Naveenkumar - Square root of a number
