29a49f39d9cead045534f7057a448d0c00748d4b
Vivekanand <vivekanandanorasolutions.com>
2023-03-24 10:51:04 +0530

    Subtraction function

--- Python/test.py
+++ Python/test.py
@@ -1,5 +1,8 @@
 # Chidambaram-Add function
 # Vivekanand -Sub Function
+def sub(num1,num2):
+    return num1-num2
+
 # Yuvashree - Multiplication
 def multiply(num1,num2):
     return num1*num2
@@ -10,3 +13,6 @@ print(multiply(2,3))
 # Hitesh - Square of a number
 # Kumarsing - Cube of a number
 # Naveenkumar - Square root of a number
+num1=int(input("Enter the number 1 : "))
+num2=int(input("Enter the number 2 : "))
+print("Subtraction of",num1,"and",num2, "is :", sub(num1,num2))
