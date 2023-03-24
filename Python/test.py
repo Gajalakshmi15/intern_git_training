04e06e9165c4b4a0fa12a2f09f90ad4c09f9252c
yuvashree <yuvashree@anorasolutions.com>
2023-03-25 02:31:59 +0530

    multiplication is added

--- Python/test.py
+++ Python/test.py
@@ -1,6 +1,11 @@
 # Chidambaram-Add function
 # Vivekanand -Sub Function
 # Yuvashree - Multiplication
+def multiply(num1,num2):
+    return num1*num2
+
+print(multiply(2,3))
+
 # Rajakeerthana - Division
 # Hitesh - Square of a number
 # Kumarsing - Cube of a number
