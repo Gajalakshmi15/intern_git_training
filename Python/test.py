539b76d56ec1e41a36fc7a5de51640ab70e5f72c
RajaKeerthana159 <128598066+RajaKeerthana159@users.noreply.github.com>
2023-03-24 12:45:17 +0530

    Update test.py

--- Python/test.py
+++ Python/test.py
@@ -17,11 +17,6 @@ def multiply(num1,num2):
 print(multiply(2,3))
 
 # Rajakeerthana - Division
-def division(num1,num2):
-    return num1%num2
-
-print(division(3,2))
-
 # Hitesh - Square of a number
 def square(num):
     return n**2
