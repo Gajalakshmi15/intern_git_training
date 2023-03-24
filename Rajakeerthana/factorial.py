88be23929ec0e7603049b23d4a8ff7fada4883f7
Raja Keerthana <rajakeerthana@anorasolutions.com>
2023-03-24 05:42:14 +0530

    Two files without change

---
+++ Rajakeerthana/factorial.py
@@ -0,0 +1,12 @@
+num=int(input("Enter the number"))
+fact=1
+for i in range(1,num+1):
+    fact=fact*i
+print(fact)
+# Pseudocode
+
+# User Input
+# Initialising the variable(fact) which starts from 1
+# For loop to iterate the numbers from 1 to num+1
+# Multiplying fact into i,so that fact value get changed for each iteration
+# Printing final output
