2ab9cf0f9f5d1deb7eeaa11b1f26928108f894aa
Vivekanand <vivekanandanorasolutions.com>
2023-03-24 10:37:22 +0530

    Subtraction fucntion

--- Python/test.py
+++ Python/test.py
@@ -10,3 +10,10 @@ print(multiply(2,3))
 # Hitesh - Square of a number
 # Kumarsing - Cube of a number
 # Naveenkumar - Square root of a number
+
+def sub(a,b):
+    return (a-b)
+
+a=int(input("Enter the number 1: "))
+b=int(input("Enter the number 2: "))
+print("Subtraction of",a, "and", b, "is :",sub(a,b))
